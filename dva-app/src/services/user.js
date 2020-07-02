0f54163c0be29c743e52b16159d8e27d01b45a67
陈南 <chennan@chennandeMacBook-Pro.local>
2020-07-02 19:07:48 +0800

    my first react project

--- dva-app/src/services/user.js
+++
@@ -1,6 +0,0 @@
-import { stringify } from 'qs';
-import request from "../utils/request";
-export async function getUser(params) {
-  console.log(params)
-  return request(`http://127.0.0.1:3000/data?${stringify(params)}`)
-}
